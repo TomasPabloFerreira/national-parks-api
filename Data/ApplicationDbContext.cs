ccdfd230fc6df8dbef5179baf0bccd971ee37c77
TomasPabloFerreira <tomasferreira373@gmail.com>
2020-09-06 19:20:35 -0300

    feat(migration): add CreateNationalParkTable migration

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using national_parks_api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace national_parks_api.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+			
+		}
+
+		public DbSet<NationalPark> NationalParks { get; set; }
+	}
+}
