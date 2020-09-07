6367b0499fe7ac7e2fee26eca9c55715f69ca2ee
TomasPabloFerreira <tomasferreira373@gmail.com>
2020-09-07 18:57:16 -0300

    feat: create trail model and trail dto

---
+++ Models/Dtos/TrailDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using static national_parks_api.Models.Trail;
+
+namespace national_parks_api.Models.Dtos
+{
+	public class TrailDto
+	{
+		public int Id { get; set; }
+		[Required]
+		public string Name { get; set; }
+		[Required]
+		public double Distance { get; set; }
+		public DifficultyType Difficulty { get; set; }
+		[Required]
+		public int NationalParkId { get; set; }
+		public NationalParkDto NationalPark { get; set; }
+	}
+}
