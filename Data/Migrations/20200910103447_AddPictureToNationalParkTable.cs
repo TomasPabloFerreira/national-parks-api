79fd05f26d8bfd251229535c147087f094fab0cc
TomasPabloFerreira <tomasferreira373@gmail.com>
2020-09-10 07:37:37 -0300

    feat(migration): add AddPictureToNationalParkTable migration

---
+++ Data/Migrations/20200910103447_AddPictureToNationalParkTable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace national_parks_api.Migrations
+{
+    public partial class AddPictureToNationalParkTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Picture",
+                table: "NationalParks",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Picture",
+                table: "NationalParks");
+        }
+    }
+}
