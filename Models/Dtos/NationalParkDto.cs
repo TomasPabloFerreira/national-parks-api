ae715bf70ef7c32d8ead74e38c8bcaefaac82f29
TomasPabloFerreira <tomasferreira373@gmail.com>
2020-09-10 07:33:42 -0300

    feat: add picture property to national park model

--- Models/Dtos/NationalParkDto.cs
+++ Models/Dtos/NationalParkDto.cs
@@ -13,6 +13,7 @@ namespace national_parks_api.Models.Dtos
 		public string Name { get; set; }
 		[Required]
 		public string State { get; set; }
+		public byte[] Picture { get; set; }
 		public DateTime Created { get; set; }
 		public DateTime Established { get; set; }
 	}
